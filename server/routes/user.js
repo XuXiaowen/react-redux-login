865ee2a2add41cd49ec12d6add5951ca277c2984
XuXiaowen <2928477161@qq.com>
2020-04-11 15:29:47 +0800

    feat: 后端验证数据

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+const isEmpty = require('lodash/isEmpty')
+const validator = require('validator')
+
+const validatorInput = (data) => {
+    let errors = {};
+    if(!validator.isLength(data.username, {min: 2, max: 30})){ 
+        errors.username = '名字长度不能小于2或超过30位'
+    }
+    if(!validator.isEmail(data.email)) {
+        error.email = '邮箱不合法'
+    }
+    if(!validator.isLength(data.password, {min: 8})){ 
+        errors.name = '密码长度不能小于8位'
+    }
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
 router.get('/', (req, res) => {
     console.log(req.query, 'req.query')
-    res.send('user get');
 });
 
 router.post("/",(req,res) =>{
     console.log(req.body, 'req.body')
-    res.send('user post');
+    const { errors, isValid } = validatorInput(req.body);
+    if(!isValid) {
+        res.status(400).json(errors);
+    }
 })
 
 module.exports = router;
